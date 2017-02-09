2ad8c03ebe036b52993e5af6da2bf957ed1aa65d
Mathias Perslev <mathiasperslev@perslev.local>
2017-02-09 20:07:51 +0100

    Now creates a HTML page of the assignment and downloads files mentioned in the description not hosted on the Canvas server

--- CanvasEntities/assignment.py
+++ CanvasEntities/assignment.py
@@ -9,10 +9,13 @@ February 2017
 
 # Inbuilt modules
 import re
+import os
+import io
 
 # CanvasSync module imports
 from CanvasSync.CanvasEntities.entity import Entity
 from CanvasSync.CanvasEntities.file import File
+from CanvasSync.CanvasEntities.linked_file import LinkedFile
 from CanvasSync.Statics.ANSI import ANSI
 from CanvasSync.Statics import static_functions
 
@@ -44,17 +47,34 @@ class Assignment(Entity):
 
     def __repr__(self):
         """ String representation, overwriting base class method """
-        return u" " * 15 + u"|   " + u"\t" * self.indent + u"%s: %s" % (ANSI.format("Assignment",
-                                                                                    formatting="assignment"),
-                                                                        self.name)
+        status = ANSI.format("[SYNCED]", formatting="green")
+        return status + u" " * 7 + u"|   " + u"\t" * self.indent + u"%s: %s" \
+                                                                   % (ANSI.format("Assignment", formatting="assignment"),
+                                                                      self.name)
+
+    def make_html(self):
+        """ Create the main HTML description page of the assignment """
+
+        # Create URL pointing to Canvas live version of the assignment
+        url = self.settings.domain_ + "/courses/%s/assignments/%s" % (self.get_parent().get_parent().get_id(),
+                                                                      self.get_id())
+
+        if not os.path.exists(self.sync_path + self.name + ".html"):
+            with io.open(self.sync_path + self.name + ".html", "w", encoding="utf-8") as out_file:
+                out_file.write(u"<h1><strong>%s</strong></h1>" % self.name)
+                out_file.write(u"<big><a href=\"%s\">Click here to open the live page in Canvas</a></big>" % url)
+                out_file.write(u"<hr>")
+                out_file.write(self.assignment_info["description"])
 
     def add_files(self):
         """ Add all files that can be found in the description of the assignment to the list of children and sync """
         # Get URL pointing to file objects described somewhere in the description section
-        file_info_urls = re.findall(r'data-api-endpoint=\"(.*?)\"', self.assignment_info["description"])
+
+        # Get file URLs pointing to Canvas items
+        canvas_file_urls = re.findall(r'data-api-endpoint=\"(.*?)\"', self.assignment_info["description"])
 
         # Download information on all found files and add File objects to the children
-        for url in file_info_urls:
+        for url in canvas_file_urls:
             try:
                 file_info = self.api.download_item_information(url)
             except Exception:
@@ -62,6 +82,21 @@ class Assignment(Entity):
 
             item = File(file_info, parent=self)
             self.add_child(item)
+
+        # We also look for links to files downloaded from other servers
+        # Get all URLs ending in a file name (determined as a ending with a '.'
+        # and then between 1 and 10 of any characters after that). This has 2 purposes:
+        # 1) We do not try to re-download Canvas server files, since they are not matched by this regex
+        # 2) We should stay clear of all links to web-sites (they could be large to download, we skip them here)
+        urls = re.findall(r'href=\"([^ ]*[.]{1}.{1,10})\"', self.assignment_info["description"])
+
+        for url in urls:
+            linked_file = LinkedFile(url, self)
+
+            if linked_file.url_is_valid():
+                self.add_child(linked_file)
+            else:
+                del linked_file
 
     def walk(self, counter):
         """ Walk by adding all File objects to the list of children """
@@ -74,12 +109,13 @@ class Assignment(Entity):
 
     def sync(self):
         """
-        1) Adding all File objects to the list of children
+        1) Adding all File and LinkedFile objects to the list of children
         2) Synchronize all children objects
         """
         print unicode(self)
 
         self.add_files()
+        self.make_html()
 
         for file in self:
             file.sync()
